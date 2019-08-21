729613e7de833a8d4c506d5cf18b4d5960442c9d
LUCAS RIBEIRO RODRIGUES <u16186@academico.cotuca.unicamp.br>
2019-08-21 13:49:44 -0300

    Add empty pages

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'React';
+import {View, Text} from 'react-native';
+
+export default class Main extends Component{
+    render(){
+        return(
+            <View>
+                <Text>This is the login page</Text>
+            </View>
+        );
+    }
+}

8a207818874a574f23767d6f28e03d889235a60f
LUCAS RIBEIRO RODRIGUES <u16186@academico.cotuca.unicamp.br>
2019-08-30 09:45:13 -0300

    Refactored component name

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,9 +4,18 @@ import {View, Text} from 'react-native';
 export default class Main extends Component{
     render(){
         return(
-            <View>
+            <View style={styles.container}>
                 <Text>This is the login page</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });

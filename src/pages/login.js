e29e143be5a765da9fd2a441fe5e177e6cda2db1
LUCAS RIBEIRO RODRIGUES <u16186@academico.cotuca.unicamp.br>
2019-08-30 13:36:29 -0300

    Add style to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,37 @@
 import React, {Component} from 'React';
-import {View, Text} from 'react-native';
+const Logo = require("../../assets/icon.png")
+import {StyleSheet, View, Text, Image} from 'react-native';
+import { Container, Header, Content, Form, Item, Label, Input, Button } from 'native-base';
 
-export default class Main extends Component{
+export default class Login extends Component{
+    
+    static navigationOptions = {
+        header: null,
+    }
+
     render(){
         return(
-            <View style={styles.container}>
-                <Text>This is the login page</Text>
-            </View>
+            <Container style={styles.container}>
+                <Content>
+                    <Form style={styles.content}>
+                        <Image source={Logo} style={{alignSelf: 'center'}}/>
+                        <Item rounded  style={styles.white}>
+                            <Input placeholder='Usuário'/>
+                        </Item>
+                        <Item rounded style={styles.white}>
+                            <Input placeholder='Senha'/>
+                        </Item>
+                        <View style={{flexDirection: "row"}}>
+                            <Button rounded style={styles.button}>
+                                <Text style={{color: "#FFF"}}>Entrar</Text>
+                            </Button>
+                            <Button transparent>
+                                <Text style={{color: "#FFF"}}>Cadastrar-se</Text>
+                            </Button>
+                        </View>
+                    </Form>
+                </Content>
+            </Container>
         );
     }
 }
@@ -14,8 +39,24 @@ export default class Main extends Component{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: '#059F9F',
+      flexDirection: "column",
+      alignItems: "center",
     },
+    content: {
+        flex: 1,
+        marginTop: 130,
+        height: 360,
+        width: 250,
+        justifyContent: "space-around",
+    },
+    button: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "#50A083",
+        marginRight: 10,
+    },
+    white: {
+        backgroundColor: "#FFF",
+    }
   });

bb531d000496a5c1cf802dfa07282bab4ca99936
Lucas Ribeiro Rodrigues <lucasr6@yahoo.com.br>
2019-08-31 22:58:30 -0300

    testing register page

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@ import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 import login from './pages/login';
 
+import register from './pages/register';
+
 const AppNavigator =  createStackNavigator({
     login,
+    //register,
 });
 
 const AppContainer = createAppContainer(AppNavigator);

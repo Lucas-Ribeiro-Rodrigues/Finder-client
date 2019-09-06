67d9cac2b895f7f44f414b6d200944da4d023769
LUCAS RIBEIRO RODRIGUES <u16186@academico.cotuca.unicamp.br>
2019-09-06 11:05:12 -0300

    Update app routing

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 
-import login from './pages/login';
-
+import login    from './pages/login';
 import register from './pages/register';
-
+import map      from './pages/map'
 const AppNavigator =  createStackNavigator({
-    login,
-    //register,
-});
+    Login   : {screen: login},
+    Register: {screen: register},
+    Map     : {screen: map} 
+}, {initialRouteName: 'Login'});
 
 const AppContainer = createAppContainer(AppNavigator);
 export default AppContainer;

486e1ec0251d9dafaab3004fb0036710c6269965
LUCAS RIBEIRO RODRIGUES <u16186@academico.cotuca.unicamp.br>
2019-09-10 13:50:03 -0300

    Add profile page

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 
-import login    from './pages/login';
-import register from './pages/register';
-import map      from './pages/map'
+import login        from './pages/SinglePages/login';
+import register     from './pages/SinglePages/register';
+import map          from './pages/Tabs/map';
+import profile      from './pages/Tabs/profile';
 const AppNavigator =  createStackNavigator({
     Login   : {screen: login},
     Register: {screen: register},
-    Map     : {screen: map} 
+    Map     : {screen: map},
+    Profile : {screen: profile}
 }, {initialRouteName: 'Map'});
 
 const AppContainer = createAppContainer(AppNavigator);

4dc55d32c56ce03dfe730294c278d2b65ee4dd9e
LUCAS RIBEIRO RODRIGUES <u16186@academico.cotuca.unicamp.br>
2019-09-18 12:46:59 -0300

    Moved some components from wizard step to wizard 'main' page

--- src/pages/SinglePages/Wizards/itemRegister/Steps/category.js
+++ src/pages/SinglePages/Wizards/itemRegister/Steps/category.js
@@ -7,32 +7,12 @@ const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 export default class Category extends Component{
 
-    state = {invalid: true, selectedItemValue: ""}
-
     constructor(props)
     {
         super(props);
     }
 
-    onValueChangeHandler = (itemValue, itemIndex) => {
-        let invalid;
-
-        if(itemIndex > 0)
-        {
-            invalid = false;
-        }
-        else
-        {
-            invalid = true;
-        }
-
-        this.setState({selectedItemValue: itemValue, invalid: invalid});
-    }
-
-    nextPreprocess = () => {
-        this.props.saveState(0,{category:this.state.selectedItemValue})
-        this.props.nextFn()
-    }
+    state = {selectedItemValue: ''};
     
     backButtonHandler = () => {
         const {navigate} = this.props.navigation;
@@ -48,69 +28,26 @@ export default class Category extends Component{
     {
         const {navigate} = this.props.navigation;
         return(
-        <Container style={{height: SCREEN_HEIGHT}}>
-            <Content contentContainerStyle={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Item picker fixedLabel style={{width: SCREEN_WIDTH - 30}}>
-                    <Picker
-                    mode="dropdown"
-                    iosIcon={<Icon name="arrow-down" />}
-                    placeholder="Tipo"
-                    placeholderStyle={{ color: "#bfc6ea" }}
-                    placeholderIconColor="#007aff"
-                    selectedValue={this.state.selectedItemValue}
-                    style={{height: 50}}
-                    onValueChange={this.onValueChangeHandler}>
-                        <Picker.Item label="Categoria" value="Categoria"/>
-                        <Picker.Item label="Eletrônico" value="Eletronico"/>
-                        <Picker.Item label="Documento" value="Documento"/>
-                        <Picker.Item label="Animal" value="Animal"/>
-                        <Picker.Item label="Vestuário" value="Vestuario"/>
-                        <Picker.Item label="Acessórios" value="Acessorios"/>
-                        <Picker.Item label="Cartões" value="Cartoes"/>
-                        <Picker.Item label="Outros" value="Outros"/>
-                    </Picker>
-                </Item>
-            </Content>
-            <Footer style={{backgroundColor: "#059F9F", flexDirection: "row", justifyContent: "flex-start"}}>
-                <TouchableOpacity 
-                                onPress={this.backButtonHandler}
-                                color="#059F9F"
-                                style={styles.buttonEnabled}>
-                    <Text style={styles.buttonEnabledText}>Voltar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                                onPress={this.nextPreprocess}
-                                disabled={this.state.invalid}
-                                style={(!this.state.invalid ? styles.buttonEnabled: styles.buttonDisabled)}
-                                ref={e => this.buttonNext = e}>
-                    <Text style={(!this.state.invalid ? styles.buttonEnabledText: styles.buttonDisabledText)}>Próximo</Text>
-                </TouchableOpacity>
-            </Footer>
-        </Container>
+            <Item picker fixedLabel style={{width: SCREEN_WIDTH - 30}}>
+                <Picker
+                mode="dropdown"
+                iosIcon={<Icon name="arrow-down" />}
+                placeholder="Tipo"
+                placeholderStyle={{ color: "#bfc6ea" }}
+                placeholderIconColor="#007aff"
+                selectedValue={this.state.selectedItemValue}
+                style={{height: 50}}
+                onValueChange={(itemValue, itemIndex) => {this.props.onValueChangeHandler(itemValue, itemIndex); this.setState({selectedItemValue: itemValue})}}>
+                    <Picker.Item label="Categoria" value="Categoria"/>
+                    <Picker.Item label="Eletrônico" value="Eletronico"/>
+                    <Picker.Item label="Documento" value="Documento"/>
+                    <Picker.Item label="Animal" value="Animal"/>
+                    <Picker.Item label="Vestuário" value="Vestuario"/>
+                    <Picker.Item label="Acessórios" value="Acessorios"/>
+                    <Picker.Item label="Cartões" value="Cartoes"/>
+                    <Picker.Item label="Outros" value="Outros"/>
+                </Picker>
+            </Item>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    buttonEnabled: {
-        width: (SCREEN_WIDTH/2),
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    buttonEnabledText: {
-        fontSize: 24,
-        fontWeight: "bold",
-        color: "white"
-    },
-    buttonDisabled: {
-        width: (SCREEN_WIDTH/2),
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#059494"
-    },
-    buttonDisabledText: {
-        fontSize: 24,
-        fontWeight: "bold",
-        color: "#e6e6e6",
-    }
-})
--- src/pages/SinglePages/Wizards/itemRegister/itemRegister.js
+++ src/pages/SinglePages/Wizards/itemRegister/itemRegister.js
@@ -1,9 +1,13 @@
 import MultiStep from 'react-native-multistep-wizard';
 import React, { Component } from "react";
-import {View} from 'react-native'
+import { View, StyleSheet, Dimensions, TouchableOpacity, Text } from 'react-native'
+import { Container, Content, Footer, Picker, Icon, Item, Label } from 'native-base';
 import Category from './Steps/category'
 /*import StepTwo from './StepTwo'
 import StepThree from './StepThree'*/
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 export default class ItemRegister extends Component{
 
@@ -11,24 +15,104 @@ export default class ItemRegister extends Component{
         super(props);
     }
 
+    state = {invalid:true, selectedItemValue: '', actualStep: 1};
+
     static navigationOptions = {
         header: null,
     }
 
     finish(wizardState){
-            
+    }
+
+    nextPreprocess = () => {
+        this.stepProps.saveState(0,{category:this.props.selectedItemValue});
+        let actualStep = this.state.actualStep++;
+        this.setState({actualStep: actualStep});
+        this.stepProps.nextFn();
+    }
+
+    backButtonHandler = () => {
+        const {navigate} = this.props.navigation;
+        Alert.alert("Tem certeza?", 
+                    "Você perderá todos os dados colocados sobre o item até agora e voltará para a página principal",
+                    [
+                        {text: "OK", onPress: () => navigate("Main")},
+                        {text: "Cancelar", style: "cancel"}
+                    ], {cancelable: true});
+    }
+
+    onValueChangeHandler = (itemValue, itemIndex) => {
+        let invalid;
+        if(itemIndex > 0)
+        {
+            invalid = false;
+        }
+        else
+        {
+            invalid = true;
         }
 
+        this.setState({selectedItemValue: itemValue, invalid: invalid});
+    }
+
     render(){
+
         const steps = [
-            {name: 'Category', component: <Category navigation = {this.props.navigation}/>},
+            {name: 'Category', component: <Category 
+                                                    navigation = {this.props.navigation} 
+                                                    ref={e => this.stepProps = e.props} 
+                                                    onValueChangeHandler={this.onValueChangeHandler} 
+                                                    selectedItemValue={this.state.selectedItemValue}/>},
             /*{name: 'StepTwo', component: <StepTwo/>},
             {name: 'StepThree', component: <StepThree/>},*/
           ];
         return(
-            <View>
-                <MultiStep steps={steps} onFinish={this.finish}/>
-            </View>
+            <Container style={{flex: 1}}>
+                <Content contentContainerStyle={{flex: 1,  justifyContent: 'center', flexDirection: 'column', marginLeft: 15}}>
+                    <MultiStep  
+                            steps={steps} 
+                            onFinish={this.finish}/>
+                </Content>
+                <Footer style={{backgroundColor: "#059F9F", flexDirection: "row", justifyContent: "flex-start"}}>
+                    <TouchableOpacity 
+                                    onPress={this.backButtonHandler}
+                                    color="#059F9F"
+                                    style={styles.buttonEnabled}>
+                        <Text style={styles.buttonEnabledText}>Voltar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                                    onPress={this.nextPreprocess}
+                                    disabled={this.state.invalid}
+                                    style={(!this.state.invalid ? styles.buttonEnabled: styles.buttonDisabled)}
+                                    ref={e => this.buttonNext = e}>
+                        <Text style={(!this.state.invalid ? styles.buttonEnabledText: styles.buttonDisabledText)}>Próximo</Text>
+                    </TouchableOpacity>
+                </Footer>
+            </Container>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    buttonEnabled: {
+        width: (SCREEN_WIDTH/2),
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonEnabledText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "white"
+    },
+    buttonDisabled: {
+        width: (SCREEN_WIDTH/2),
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#059494"
+    },
+    buttonDisabledText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#e6e6e6",
+    }
+})

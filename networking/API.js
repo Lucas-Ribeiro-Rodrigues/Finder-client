88970d797d39b5649d1f1bbd16a8df80bf55df05
Leonardo Iwashima <30930817+lminoru@users.noreply.github.com>
2019-09-24 13:38:35 -0300

    beg-getUserData

--- networking/API.js
+++ networking/API.js
@@ -14,6 +14,10 @@ const userLogin = (email, pass)  => {
         .then(response => response.data)
 }
 
+const getUserData = (email) => {
+    return axios.get('')
+} 
+
 module.exports = {
     postUser,
     userLogin

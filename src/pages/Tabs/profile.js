ca8a696ca753b32fc4c8547e280b593836d7cc5d
Leonardo Iwashima <30930817+lminoru@users.noreply.github.com>
2019-09-25 11:51:26 -0300

    Front-end profile (getUserData())

--- src/pages/Tabs/profile.js
+++ src/pages/Tabs/profile.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'React';
 import { View, Text } from 'react-native';
+import {getUserData} from '../../../networking/API';
+
+
 
 export default class Profile extends Component{
+    state = {userData: undefined};
+    constructor(props){
+        super(props);
+        console.log('constructor');
+        getUserData('adaw')
+            .then(value => {
+                this.setState({userData: value});
+            })
+
+    }
 
     render()
     {
         return(
-            <Text>Página de perfil</Text>
+            <View>
+                <Text>Página de perfil</Text>
+                <Text>Email: {this.state.userData? this.state.userData.Email:null}</Text>
+                <Text>Nome: {this.state.userData? this.state.userData.Name:null}</Text>
+            </View>
         )
     }
 }

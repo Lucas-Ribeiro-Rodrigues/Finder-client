2bbedb01ea638ed1c0c3f088e0965155d58b9bf8
Lucas Ribeiro Rodrigues <lucasr6@yahoo.com.br>
2019-10-01 16:50:33 -0300

    Bug Fix

--- src/pages/SinglePages/Wizards/itemRegister/Steps/textInputStepGenerator.js
+++ src/pages/SinglePages/Wizards/itemRegister/Steps/textInputStepGenerator.js
@@ -14,15 +14,42 @@ export default class TextInputStepGenerator extends Component{
         })
     }
 
+    componentDidMount()
+    {
+        this.multiline      = false;
+        this.numberOfLines  = 1;
+    }
+
+    componentDidUpdate(prevProps)
+    {
+        if(prevProps != this.props)
+        {
+            if(this.props.isTextArea)
+            {
+                this.multiline      = true;
+                this.numberOfLines  = 10;
+            }
+            else
+            {
+                this.multiline      = false;
+                this.numberOfLines  = 1;
+            }
+            this.textInput.clear();
+        }
+    }
+
     render()
     {
         return(
             <View style={{flex: 1, alignSelf: "center", justifyContent: "center", width: SCREEN_WIDTH - 20}}>
                 <TextInput
-                    onChangeText={this.onValueChangeHandler}
-                    style={styles.TextInput}
                     autoFocus
-                    autoCorrect={false}/>
+                    multiline       = {this.multiline}
+                    numberOfLines   = {this.numberOfLines}
+                    ref             = {e => this.textInput = e}
+                    onChangeText    = {this.onValueChangeHandler}
+                    style           = {(!this.props.isTextArea ? styles.TextInput: {...styles.TextInput,...styles.TextArea})}
+                    autoCorrect     = {false}/>
             </View>
         )
     }
@@ -31,13 +58,18 @@ export default class TextInputStepGenerator extends Component{
 const styles = StyleSheet.create({
     TextInput:{
         borderRadius: 6,
-        borderWidth:  2,
-        height:       40,
-        width: SCREEN_WIDTH - 20,
+        borderWidth :  2,
+        height      :       40,
+        width       : SCREEN_WIDTH - 20,
         paddingRight: 10,
-        paddingLeft: 10,
-        fontSize:     20,
-        alignSelf: "center",
-        borderColor: '#d6d7da',
+        paddingLeft : 10,
+        fontSize    :     20,
+        alignSelf   : "center",
+        borderColor : '#d6d7da'
+    },
+    TextArea: {
+        height              : 400, 
+        textAlignVertical   : 'top', 
+        paddingVertical     : 10
     }
 })

5c70931427a11a09a703a4587a3a41c1fc54eef7
Lucas Ribeiro Rodrigues <lucasr6@yahoo.com.br>
2019-10-01 16:50:22 -0300

    Add new file

---
+++ src/pages/SinglePages/Wizards/itemRegister/Steps/imagePickerStepGenerator.js
@@ -0,0 +1,14 @@
+import React, { Component }             from 'react';
+import { View, TextInput, StyleSheet, Image, Dimensions }  from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+export default class ImagePickerStepGenerator extends Component{
+
+    render()
+    {
+        return(
+            <Image/>
+        )
+    }
+}

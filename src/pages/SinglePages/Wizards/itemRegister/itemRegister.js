0bd8b63ce952b9b2f7747e85893794f54c402ce1
Lucas Ribeiro Rodrigues <lucasr6@yahoo.com.br>
2019-10-01 16:41:51 -0300

    Add back button handler

--- src/pages/SinglePages/Wizards/itemRegister/itemRegister.js
+++ src/pages/SinglePages/Wizards/itemRegister/itemRegister.js
@@ -1,7 +1,6 @@
 import React, { Component }                                         from "react";
 import { StyleSheet, Dimensions, TouchableOpacity, Text, Alert }    from 'react-native'
 import { Container, Content, Footer }                               from 'native-base';
-import Category                                                     from './Steps/category';
 import MultipleChoiceStepGenerator                                  from './Steps/multipleChoiceStepGenerator';
 import TextInputStepGenerator                                       from './Steps/textInputStepGenerator';
 import stepsJson                                                    from './stepsInfo.json';
@@ -10,14 +9,26 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 export default class ItemRegister extends Component{
-    stepsStack = [];
 
     constructor(props){
         super(props);
+        this.initAttributes();
+    }
+
+    initAttributes()
+    {
         this.ActualStep             =  MultipleChoiceStepGenerator;
         this.actualQuestionName     = "Category";
+        this.stepLabel              = "Categoria"
         this.stepContent            = stepsJson[this.actualQuestionName];
         this.actualQuestionOptions  = Object.keys(this.stepContent);
+        this.steps                  = [{
+            "Type"      :   "MultipleChoice",
+            "Options"   :   this.actualQuestionOptions,
+            "Label"     :   this.stepLabel,
+            "Name"      :   this.actualQuestionName,
+        }];
+        this.isTextArea             = false;
     }
 
     state = {invalid:false, actualStep: -1, answers: {}};
@@ -50,14 +61,27 @@ export default class ItemRegister extends Component{
         });
     }
 
-    getActualQuestion(questions, stepContent, category, actualQuestionName, actualStep){
-        if(!questions) // questions == undefined
-        {
-            questions = stepContent[category];
+    addGenericQuestions()
+    {
+        let genericQuestions = {};
+        genericQuestions["Details"] = {
+            "Type"  : "TextArea",
+            "Label" : "Detalhes"
         }
-        this.actualQuestionName = Object.keys(questions)[actualStep];
+
+        this.questions = Object.assign(this.questions, genericQuestions);
+    }
+
+    getActualQuestion(stepContent, category, actualStep){
+        if(!this.questions) // questions == undefined
+        {
+            this.questions = stepContent[category];
+            this.addGenericQuestions();
+            delete this.questions["Label"];
+        }
+        this.actualQuestionName = Object.keys(this.questions)[actualStep];
         this.setState({actualStep: actualStep});
-        return questions[this.actualQuestionName];
+        return this.questions[this.actualQuestionName];
     }
 
     getQuestionOptions(actualQuestion, actualQuestionName)
@@ -69,27 +93,41 @@ export default class ItemRegister extends Component{
         if(!this.category)
             this.category = this.step.state.value;
 
-        
         this.handleNewAnswer(this.state.answers, this.actualQuestionName, this.step.state.value, this.state.actualStep++);
 
-        let actualQuestion = this.getActualQuestion(this.questions, this.stepContent, this.category, this.actualQuestionName, this.state.actualStep);
-        this.props.navigation.setParams({stepLabel: actualQuestion["Label"]});
-        if(actualQuestion["Type"] === "MultipleChoice")
+        let actualQuestion = this.getActualQuestion(this.stepContent, this.category, this.state.actualStep);
+        
+        this.stepLabel = actualQuestion["Label"]
+        this.props.navigation.setParams({stepLabel: this.stepLabel});
+
+        switch(actualQuestion["Type"])
         {
-            this.actualQuestionOptions  = this.getQuestionOptions(actualQuestion, this.actualQuestionName);
-            this.ActualStep = MultipleChoiceStepGenerator;
+            case "MultipleChoice": {
+                this.ActualStep = MultipleChoiceStepGenerator;
+                this.actualQuestionOptions  = this.getQuestionOptions(actualQuestion, this.actualQuestionName);
+                break;
+            }
+            case "TextInput": {
+                this.ActualStep = TextInputStepGenerator;
+                this.isTextArea = false;
+                break;
+            }
+            case "TextArea": {
+                this.ActualStep = TextInputStepGenerator;
+                this.isTextArea = true;
+            }
         }
-        else
-        {
-            this.ActualStep = TextInputStepGenerator;
-        }
+
+        actualQuestion["Name"] = this.actualQuestionName;
+        this.steps.push(actualQuestion);
     }
 
     backButtonHandler = () => {
-        const {navigate} = this.props.navigation;
-        let actualStep = this.state.actualStep;
-        if(actualStep == 0)
+        this.steps.pop();
+
+        if(this.steps.length === 0)
         {
+            const {navigate} = this.props.navigation;
             Alert.alert("Tem certeza?", 
                         "Você perderá todos os dados colocados sobre o item até agora e voltará para a página principal",
                         [
@@ -99,8 +137,41 @@ export default class ItemRegister extends Component{
         }
         else
         {
-            this.setState({actualStep: actualStep--});
+            let actualStep = this.state.actualStep;
+            let actualStepObj = this.steps[actualStep];
+            actualStep--;
 
+            if(actualStep == -1)
+            {
+                this.category = undefined;
+                this.questions = undefined;
+            }
+
+            switch(actualStepObj["Type"])
+            {
+                case "MultipleChoice": {
+                    this.ActualStep = MultipleChoiceStepGenerator;
+                    break;
+                }
+                case "TextInput": {
+                    this.ActualStep = TextInputStepGenerator;
+                    this.isTextArea = false;
+                    break;
+                }
+                case "TextArea": {
+                    this.ActualStep = TextInputStepGenerator;
+                    this.isTextArea = true;
+                }
+            }
+            this.stepLabel = actualStepObj["Label"];
+            this.actualQuestionOptions = actualStepObj["Options"];
+            
+            let answers = this.state.answers;
+            delete answers[this.actualQuestionName];
+
+            this.actualQuestionName = actualStepObj["Name"];
+            this.props.navigation.setParams({stepLabel: this.stepLabel});
+            this.setState({actualStep: actualStep, answers: answers});
         }
     }
 
@@ -108,7 +179,10 @@ export default class ItemRegister extends Component{
         return(
             <Container contentContainerStyle={{flex: 1}}>
                 <Content contentContainerStyle={{flex: 1}}>
-                    <this.ActualStep ref={e => this.step = e} options={this.actualQuestionOptions}/>
+                    <this.ActualStep 
+                        ref         ={e => this.step = e} 
+                        options     ={this.actualQuestionOptions}
+                        isTextArea  ={this.isTextArea}/>
                 </Content>
                 <Footer style={{backgroundColor: "#059F9F", flexDirection: "row", justifyContent: "flex-start"}}>
                     <TouchableOpacity 

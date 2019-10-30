04fa45f65f4cb929735c6d3d80ae549b79c3310e
LUCAS RIBEIRO RODRIGUES <u16186@academico.cotuca.unicamp.br>
2019-10-30 09:17:44 -0300

    Conflicts solved

--- src/components/items/itemsList.js
+++ src/components/items/itemsList.js
@@ -4,10 +4,6 @@ import { Body, Card, CardItem, Container, Content, Header, ListItem, Left, Right
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
-<<<<<<< HEAD
-//const subcat = require("../../../assets/subcategory/");
-=======
->>>>>>> 042b17eec0af38515e3875dbd52909d07e1d63d9
 
 export default class ItemsList extends Component{
     constructor(props){
@@ -31,16 +27,10 @@ export default class ItemsList extends Component{
                 return (
                     <TouchableOpacity key={i} buttonIndex={i++} onPress={()=>this.props.utils.onPressHandler(item)}>
                         <Card>
-<<<<<<< HEAD
-                            <CardItem>
-                                {/*<Thumbnail source={{uri:"../../../assets/subcategory/iphone11.jpg"}} />*/}
-
-=======
                             <ListItem icon>
                                 <Left>
                                     <Icon active name={this.icons[item.Category]} size={25} color="#999" />
                                 </Left>
->>>>>>> 042b17eec0af38515e3875dbd52909d07e1d63d9
                                 <Body>
                                     <Text>Item: {item.Subcategory? item.Subcategory: "Indefinido"}</Text>
                                     <Text>Categoria: {item.Category? item.Category: "Indefinido"}</Text>

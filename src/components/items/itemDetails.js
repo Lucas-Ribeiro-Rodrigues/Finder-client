30d037939dfedc29d5ee47a9a958fb9019381f60
Leonardo Iwashima <30930817+lminoru@users.noreply.github.com>
2019-11-05 11:46:07 -0300

    show item information (photo, infos)

--- src/components/items/itemDetails.js
+++ src/components/items/itemDetails.js
@@ -15,21 +15,37 @@ export default class ItemDetails extends Component{
             <Container>
                 <Content>
                     <Card style={{flex: 0}}>
-                        <CardItem>
-                            <Left>
-                                <Body>
-                                    <Text>{this.props.utils.item.Subcategory}</Text>
-                                    <Text note>{this.props.utils.item.Category}</Text>
-                                </Body>
-                            </Left>
-                        </CardItem>
-
+                        
                         <CardItem>
                             <Body>
-                                <Image source={require('../../../assets/items/mali.jpeg')} style={{height: 200, width: 200, flex: 1}}/>
-                                <Text>{this.props.utils.item.Details}</Text>
+
+                                <Image source={require('../../../assets/items/mali.jpeg')} style={{height: 200, width: 200, flex: 1,  alignSelf: "center" }}/> 
                             </Body>
                         </CardItem>
+
+                        <CardItem header>
+                            <Body>
+                                <Text style={{fontWeight: 'bold'}}>{this.props.utils.item.Subcategory}</Text>
+                                <Text note>{this.props.utils.item.Category}</Text>
+                            </Body>
+                        </CardItem>
+                    
+
+
+                        <CardItem header button onPress={() => alert("leo.minoru@hotmail.com")}>
+                        <Text>Informações de contato</Text>
+                        </CardItem>
+                        <CardItem button onPress={() => alert("mostrar no mapa")}>
+                        <Body>
+                            <Text>
+                            Jardim Chapadão
+                            </Text>
+                        </Body>
+                        </CardItem>
+                        <CardItem footer button onPress={() => alert("Ligar")}>
+                        <Text>(19)982782531</Text>
+                        </CardItem>
+
 
                         <CardItem>
                             <Left>
